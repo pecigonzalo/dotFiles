db483d17524e526f07ecd22aecf6a218f34e1c73
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-02-21 12:57:29 +0100

    Support zst for ex function

--- functions.zsh
+++ functions.zsh
@@ -16,6 +16,7 @@ ex() {
     *.tar) tar xvf "$1" ;;
     *.tbz2) tar xvjf "$1" ;;
     *.tgz) tar xvzf "$1" ;;
+    *.zst) tar --zstd -xvf "$1" ;;
     *.zip) unzip "$1" ;;
     *.Z) uncompress "$1" ;;
     *.7z) 7z x "$1" ;;
