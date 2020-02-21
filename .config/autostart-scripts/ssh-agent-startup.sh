73f443cf6f0da72bdf673f18f35ed1d3f83b3f0f
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-02-21 12:29:20 +0100

    Set ssh-agent-startup defaults

--- .config/autostart-scripts/ssh-agent-startup.sh
+++ .config/autostart-scripts/ssh-agent-startup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
-export SSH_ASKPASS="/usr/bin/ksshaskpass"
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR:-/tmp}/ssh-agent.socket"
+
+if [[ -f /usr/bin/ksshaskpass ]]; then
+  export SSH_ASKPASS="/usr/bin/ksshaskpass"
+fi
 
 ssh-add </dev/null
 ssh-add "$HOME/.ssh/pecigonzalo_ed25519" </dev/null
 ssh-add "$HOME/.ssh/pecigonzalo_rsa" </dev/null
-ssh-add "$HOME/.ssh/blessid"
-
+ssh-add "$HOME/.ssh/blessid" </dev/null
