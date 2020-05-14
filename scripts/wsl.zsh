15e5f9be129396866bb030d28b5d7b35d7a4a551
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-05-14 10:08:49 +0200

    Ignore paths and .dll in WSL

--- scripts/wsl.zsh
+++ scripts/wsl.zsh
@@ -1,5 +1,24 @@
 #!/usr/bin/zsh
 
+# Manually set PATH for Windows folders we want
+path=(
+  $path
+  "/mnt/c/Windows/system32"
+  "/mnt/c/Windows"
+  # /mnt/c/Windows/System32/Wbem
+  "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/"
+  "/mnt/c/Windows/System32/OpenSSH/"
+  "/mnt/c/Program Files/Microsoft VS Code/bin"
+  "/mnt/c/Users/pecigonzalo/go/bin"
+  "/mnt/c/Users/pecigonzalo/scoop/shims"
+  "/mnt/c/Users/pecigonzalo/AppData/Local/Microsoft/WindowsApps"
+  "/mnt/c/Users/pecigonzalo/bin"
+)
+
+# Remove .dll and other crap from autocomp
+zstyle ':completion:*:complete:-command-::commands' ignored-patterns '*.dll' '*.DLL'
+
+
 # If not under WSL, return
 if [[ -z "$WSL_DISTRO_NAME" ]]; then
   return
