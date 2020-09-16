3702efed33f7a995fd2b898218f875b2ec4ab853
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-09-16 12:06:30 +0200

    Rework zinit scripts to the new format

--- scripts/zinit.zsh
+++ scripts/zinit.zsh
@@ -1,8 +1,4 @@
 #!/usr/bin/zsh
-source ~/.zinit/bin/zinit.zsh
-
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
 
 ## SSH
 zstyle :omz:plugins:ssh-agent identities pecigonzalo_ed25519 pecigonzalo_rsa
@@ -10,9 +6,11 @@ zinit snippet OMZ::plugins/ssh-agent/ssh-agent.plugin.zsh
 
 ## Programs
 # Load asdf
-ASDF_DIR="${HOMEBREW_PREFIX}/opt/asdf"
-zinit ice blockf
-zinit snippet OMZ::plugins/asdf/asdf.plugin.zsh
+zinit light-mode for \
+  @asdf-vm/asdf
+
+zinit is-snippet for \
+  blockf "$(asdf where gcloud)/completion.zsh.inc"
 
 # Direnv
 zinit ice wait lucid as"null" \
@@ -20,75 +18,55 @@ zinit ice wait lucid as"null" \
 zinit snippet "${HOMEBREW_PREFIX}/bin/direnv"
 
 ## OMZ Config
-zinit snippet OMZ::lib/key-bindings.zsh
-zinit snippet OMZ::lib/git.zsh
-zinit snippet OMZ::lib/clipboard.zsh
-zinit snippet OMZ::lib/termsupport.zsh
-
-## Completions
-zinit ice wait lucid blockf atpull'zinit creinstall -q .'
-zinit light zsh-users/zsh-completions
-
-zinit ice as"completion"
-zinit snippet OMZ::plugins/terraform/_terraform
+zinit for \
+  OMZ::lib/key-bindings.zsh \
+  OMZ::lib/clipboard.zsh \
+  OMZ::lib/termsupport.zsh
 
 ## Others
 # fzf
-zinit snippet "${HOMEBREW_PREFIX}/opt/fzf/shell/key-bindings.zsh"
-zinit snippet "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.zsh"
+zinit is-snippet for \
+  "${HOMEBREW_PREFIX}/opt/fzf/shell/key-bindings.zsh" \
+  "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.zsh"
 
 # Git
-zinit ice wait atload"unalias grv" lucid
-zinit snippet OMZ::plugins/git/git.plugin.zsh
+zinit wait lucid for \
+  OMZ::lib/git.zsh \
+  atload:"unalias grv" OMZ::plugins/git/git.plugin.zsh \
+  OMZ::plugins/git-auto-fetch/git-auto-fetch.plugin.zsh \
+  OMZ::plugins/gitignore/gitignore.plugin.zsh \
+  OMZ::plugins/git-flow/git-flow.plugin.zsh
 
-zinit snippet OMZ::plugins/git-auto-fetch/git-auto-fetch.plugin.zsh
-zinit snippet OMZ::plugins/gitignore/gitignore.plugin.zsh
-zinit snippet OMZ::plugins/git-flow/git-flow.plugin.zsh
+# Terraform
+zinit ice as:"completion"
+zinit snippet OMZ::plugins/terraform/_terraform
 
 # Python
-zinit ice wait lucid blockf
-zinit snippet OMZ::plugins/pip/pip.plugin.zsh
-
-zinit ice as"completion"
-zinit snippet OMZ::plugins/pip/_pip
-
-zinit ice blockf wait lucid as"null" \
-  atclone'pyenv init - > zpyenv.zsh' atpull'%atclone' src"zpyenv.zsh"
-zinit snippet "${HOMEBREW_PREFIX}/opt/pyenv/completions/pyenv.zsh"
+zinit wait lucid for \
+  blockf OMZ::plugins/pip/pip.plugin.zsh \
+  as:"completion" OMZ::plugins/pip/_pip \
+  OMZ::plugins/pyenv/pyenv.plugin.zsh
 
 # Docker
-zinit snippet OMZ::plugins/docker-compose/docker-compose.plugin.zsh
-
-zinit ice wait as"completion" lucid
-zinit snippet OMZ::plugins/docker/_docker
+zinit wait lucid for \
+  OMZ::plugins/docker-compose/docker-compose.plugin.zsh \
+  as:"completion" OMZ::plugins/docker/_docker
 
 # Utils
-zinit snippet OMZ::plugins/common-aliases/common-aliases.plugin.zsh
-zinit snippet OMZ::plugins/sudo/sudo.plugin.zsh
-zinit snippet OMZ::plugins/rsync/rsync.plugin.zsh
-zinit snippet OMZ::plugins/urltools/urltools.plugin.zsh
+zinit wait lucid for \
+  atload:"unalias fd" OMZ::plugins/common-aliases/common-aliases.plugin.zsh \
+  OMZ::plugins/sudo/sudo.plugin.zsh \
+  OMZ::plugins/rsync/rsync.plugin.zsh \
+  OMZ::plugins/urltools/urltools.plugin.zsh
 
-# K
-zinit ice wait lucid blockf
-zinit light rimraf/k
-
-# Z
-zinit ice blockf
-zinit light rupa/z
-zinit ice blockf
-zinit light changyuheng/fz
-
-# Tipz
-zinit ice wait lucid
-zinit light "molovo/tipz"
-
-# Interactive CD
-zinit ice wait lucid
-zinit light "changyuheng/zsh-interactive-cd"
-
-# forgit
-zinit ice wait lucid
-zinit light "wfxr/forgit"
+# K, Z
+zinit wait lucid blockf light-mode for \
+  rimraf/k \
+  rupa/z \
+  changyuheng/fz \
+  changyuheng/zsh-interactive-cd \
+  wfxr/forgit \
+  molovo/tipz
 
 ## Theme
 # zinit light denysdovhan/spaceship-prompt
@@ -96,20 +74,10 @@ eval "$(starship init zsh)"
 
 ## Last
 # Load completions with highlighting
-zinit ice wait lucid atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay"
-zinit light zsh-users/zsh-syntax-highlighting
-# zini light zdharma/fast-syntax-highlighting
-
-zinit ice wait lucid atload"_zsh_autosuggest_start"
-zinit light zsh-users/zsh-autosuggestions
-
-# Fish like search
-zinit ice wait lucid atload' \
-  [ -n "${terminfo[kcuu1]}" ] && bindkey "${terminfo[kcuu1]}" history-substring-search-up; \
-  [ -n "${terminfo[kcud1]}" ] && bindkey "${terminfo[kcud1]}" history-substring-search-down; \
-  bindkey -M emacs "^P" history-substring-search-up; \
-  bindkey -M emacs "^N" history-substring-search-down; \
-  bindkey -M vicmd "k" history-substring-search-up; \
-  bindkey -M vicmd "j" history-substring-search-down; \
-'
-zinit light zsh-users/zsh-history-substring-search
+zinit wait lucid for \
+  atinit:"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" \
+  zsh-users/zsh-syntax-highlighting \
+  atload:"!_zsh_autosuggest_start" \
+  zsh-users/zsh-autosuggestions \
+  blockf \
+  zsh-users/zsh-completions
