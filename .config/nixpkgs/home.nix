aaff7b3e770040d322732b34b3d214c05042bd3c
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2021-09-24 10:33:06 +0200

    Migrate Starship config

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,4 +1,5 @@
-{ pkgs
+{ lib
+, pkgs
 , ...
 }:
 let
@@ -188,6 +189,82 @@ in
       SHOW_AWS_PROMPT = "false"; # Disable OMZ prompt
     };
   };
+  programs.starship = {
+    enable = true;
+
+    settings = {
+      add_newline = true;
+
+      directory = {
+        style = "fg:blue";
+        truncation_length = 0;
+        truncate_to_repo = true;
+      };
+
+      git_branch = {
+        style = "fg:240";
+      };
+
+      aws = {
+        format = "on [$symbol$profile] ($style) ";
+      };
+
+      gcloud = {
+        format = "on [$symbol$project(\($region\))]($style) ";
+        region_aliases = {
+          us-central1 = "uc1";
+        };
+      };
+
+      kubernetes = {
+        disabled = false;
+        symbol = "⛵ ";
+        format = "on [$symbol$context(\($namespace\))]($style) ";
+      };
+
+      nix_shell = {
+        impure_msg = "";
+        format = "via [❄️ $state( \($name\))](bold blue) ";
+      };
+
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_status"
+        "$hg_branch"
+        "$docker_context"
+        "$package"
+        "$dotnet"
+        "$elixir"
+        "$elm"
+        "$golang"
+        "$haskell"
+        "$java"
+        "$nodejs"
+        "$python"
+        "$ruby"
+        "$rust"
+        "$kubernetes"
+        "$terraform"
+        "$nix_shell"
+        "$memory_usage"
+        "$gcloud"
+        "$aws"
+        "$env_var"
+        "$cmd_duration"
+        "$custom"
+        "$line_break"
+        "$jobs"
+        "$battery"
+        "$time"
+        "$character"
+      ];
+    };
+  };
 
   programs.fzf = {
     enable = true;
@@ -350,6 +427,7 @@ in
 
   programs.neovim = {
     enable = true;
+    viAlias = true;
     vimAlias = true;
 
     plugins = with pkgs.vimPlugins; [
@@ -613,9 +691,6 @@ in
 
     # GCP
     berglas
-
-    # Console
-    starship
 
     # Node
     nodejs
