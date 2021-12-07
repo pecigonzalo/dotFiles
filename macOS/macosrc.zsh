aed11b0e695e288fd5debc4bee4935d47aad2ed4
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2021-12-07 11:11:46 +0100

    Removed unused zsh blocks

--- macOS/macosrc.zsh
+++ macOS/macosrc.zsh
@@ -1,7 +1,1 @@
 #!/usr/bin/zsh
-
-# alias asdf="arch -x86_64 asdf"
-
-# export CFLAGS="${CFLAGS} -I$(xcrun --show-sdk-path)/usr/include"
-# export CPPFLAGS="${CPPFLAGS} -I$(xcrun --show-sdk-path)/usr/include"
-# export CPATH="${CPATH} -I$(xcrun --show-sdk-path)/usr/include"
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,54 +1,5 @@
 #!/usr/bin/zsh
 
-# Linuxbrew or Homebrew
-# if [[ -d /home/linuxbrew/.linuxbrew ]]; then
-#   export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
-# elif [[ -d /opt/homebrew ]]; then
-#   export HOMEBREW_PREFIX="/opt/homebrew"
-# fi
-
-# if [[ -n $HOMEBREW_PREFIX ]]; then
-#   export HOMEBREW_CELLAR="${HOMEBREW_PREFIX}/Cellar"
-#   export HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX}/Homebrew"
-#   export path=("${HOMEBREW_PREFIX}/bin" "${HOMEBREW_PREFIX}/sbin" $path)
-#   export MANPATH="${HOMEBREW_PREFIX}/share/man${MANPATH+:$MANPATH}:"
-#   export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH}"
-#   FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}"
-# fi
-# Homebrew gnubins
-# if [[ -d /opt/homebrew ]]; then
-# for gnubin in ${HOMEBREW_PREFIX}/opt/*/libexec/gnubin; do
-#   export path=($gnubin $path)
-# done
-# for gnuman in ${HOMEBREW_PREFIX}/opt/*/libexec/gnuman; do
-#   export manpath=($gnuman $manpath)
-# done
-# excl=("/opt/homebrew/opt/findutils/libexec/gnubin" "/opt/homebrew/opt/coreutils/libexec/gnubin")
-# path=${path:|excl}
-# export LDFLAGS="-L${HOMEBREW_PREFIX}/opt/zlib/lib -L${HOMEBREW_PREFIX}/opt/bzip2/lib"
-# export CPPFLAGS="-I${HOMEBREW_PREFIX}/opt/zlib/include -I${HOMEBREW_PREFIX}/opt/bzip2/include"
-# export PKG_CONFIG_PATH="${HOMEBREW_PREFIX}/opt/zlib/lib/pkgconfig"
-# fi
-
-# Nix
-# if [[ -f "${HOME}/.nix-profile/etc/profile.d/nix.sh" ]]; then
-#   source "${HOME}/.nix-profile/etc/profile.d/nix.sh"
-#   source "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
-#   FPATH="${HOME}/.nix-profile/share/zsh/site-functions:${FPATH}"
-#   FPATH="${HOME}/.nix-profile/share/zsh/vendor-completions:${FPATH}"
-# fi
-
-# if [[ -d ${HOME}/.nix-profile ]]; then
-#   export PKG_CONFIG_PATH="${HOME}/.nix-profile/lib/pkgconfig"
-#   export CFLAGS="-I${HOME}/.nix-profile/include"
-#   export CPPFLAGS="-I${HOME}/.nix-profile/include"
-#   export CPATH="${HOME}/.nix-profile/include"
-#   export LIBRARY_PATH="${HOME}/.nix-profile/lib"
-#   export LDFLAGS="-L${HOME}/.nix-profile/lib"
-#   export LD_LIBRARY_PATH="${HOME}/.nix-profile/lib"
-#   export QTDIR="${HOME}/.nix-profile"
-# fi
-
 # Set LS_COLORS
 eval $(dircolors)
 
