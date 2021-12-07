c5d0d4d369b6021e4069bd4c4fb654eb96b7d707
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2021-12-07 10:24:08 +0100

    Update macOs defaults

--- nix/darwin/defaults.nix
+++ nix/darwin/defaults.nix
@@ -1,19 +1,47 @@
 { ... }:
 {
   system.defaults.dock = {
-    autohide = true;
     orientation = "left";
-    tilesize = 45;
+    autohide = true;
+    tilesize = 64;
     show-recents = true;
+
+    mineffect = "genie";
+    minimize-to-application = true;
+
+    mru-spaces = false;
+  };
+
+  system.defaults.spaces = {
+    spans-displays = false;
   };
 
   system.defaults.NSGlobalDomain = {
-    KeyRepeat = 2;
     InitialKeyRepeat = 15;
+    KeyRepeat = 2;
+
+    NSAutomaticCapitalizationEnabled = false;
+    NSAutomaticDashSubstitutionEnabled = false;
+    NSAutomaticPeriodSubstitutionEnabled = false;
+    NSAutomaticQuoteSubstitutionEnabled = false;
+    NSAutomaticSpellingCorrectionEnabled = true;
+
+    NSScrollAnimationEnabled = true;
+
+    _HIHideMenuBar = false;
+
+    AppleShowAllExtensions = true;
   };
 
   system.defaults.finder = {
     AppleShowAllExtensions = true;
     _FXShowPosixPathInTitle = true;
+  };
+
+  system.defaults.trackpad = {
+    ActuationStrength = 1;
+    Clicking  = true;
+    FirstClickThreshold = 2;
+    SecondClickThreshold = 2;
   };
 }
