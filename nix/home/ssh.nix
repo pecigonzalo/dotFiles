d4045645f3dfc189bd721d4a69c48a51857e3dd7
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2021-12-15 10:02:38 +0100

    Remove ssh key workaround

--- nix/home/ssh.nix
+++ nix/home/ssh.nix
@@ -12,12 +12,6 @@
     includes = [
       "*.config"
     ];
-
-    # TODO: Remove and move to per-host
-    extraConfig = ''
-      HostkeyAlgorithms +ssh-rsa-cert-v01@openssh.com
-      # PubkeyAcceptedAlgorithms +ssh-rsa-cert-v01@openssh.com
-    '';
   };
 
   # TODO: We have to disable openssh as ssh-add does not work with macOS
