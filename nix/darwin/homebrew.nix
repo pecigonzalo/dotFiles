1147fea85b1cdb1347b57c0fe8b86d050f630431
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2022-04-05 15:44:20 +0200

    Fix homebrew reference for non-m1

--- nix/darwin/homebrew.nix
+++ nix/darwin/homebrew.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 let
-  brewPrefix = "/opt/homebrew/bin";
+  brewBinPrefix = if pkgs.system == "aarch64-darwin" then "/opt/homebrew/bin" else "/usr/local/bin";
 in
 {
   environment.shellInit = ''
