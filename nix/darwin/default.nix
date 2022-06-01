e2bfaac7fc232689af02e360c8d9ceb8b545acac
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2022-06-01 15:42:34 +0200

    Disable TERMINFO

--- nix/darwin/default.nix
+++ nix/darwin/default.nix
@@ -38,6 +38,6 @@
 
   # https://github.com/nix-community/home-manager/issues/423
   environment.variables = {
-    TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
+    # TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
   };
 }
