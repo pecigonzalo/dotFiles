5789a6a06e52e6c14424d1ba602c7735fb55977f
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2022-07-17 11:47:42 +0200

    Remove nix-index pin

--- nix/overlays/m1.nix
+++ nix/overlays/m1.nix
@@ -18,7 +18,6 @@ if isSillicon then {
     # https://github.com/vic/mk-darwin-system/blob/main/modules/intel-overlay.nix#L13
     llvmPackages_5 llvmPackages_6 llvmPackages_7 llvmPackages_8 llvmPackages_9 llvmPackages_10
     asdf-vm
-    nix-index
     ;
   inherit (final.pkgs-x86-stable)
     packer
