b02983eb5c4e4df2228cb945e6d40a17fb9189ef
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2022-08-30 12:13:05 -0300

    Enable nvim cmp shortcuts

--- nix/home/neovim/cmp.lua
+++ nix/home/neovim/cmp.lua
@@ -10,13 +10,13 @@ cmp.setup({
     -- completion = cmp.config.window.bordered(),
     -- documentation = cmp.config.window.bordered(),
   },
-  -- mapping = cmp.mapping.preset.insert({
-  --   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  --   ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  --   ['<C-Space>'] = cmp.mapping.complete(),
-  --   ['<C-e>'] = cmp.mapping.abort(),
-  --   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  -- }),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'vsnip' }, -- For vsnip users.
@@ -47,5 +47,8 @@ cmp.setup.cmdline(':', {
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require('lspconfig').terraformls.setup {
+  capabilities = capabilities
+}
+require('lspconfig').kotlin_language_server.setup {
   capabilities = capabilities
 }
