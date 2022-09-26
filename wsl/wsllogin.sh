228e08542c9966ab61f8d85290c81a4366bd99bb
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2022-09-26 21:23:54 +0200

    Update configuration for WSL2 supporting systmed

--- wsl/wsllogin.sh
+++ wsl/wsllogin.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
-services=(
-  cron
-  rsyslog
-  docker
-)
-
-for service in "${services[@]}"; do
-  if [[ $(service "$service" status) =~ .*"is not running" ]]; then
-    sudo service "$service" start
-  fi
-done
-
-# X Forwarding
-DISPLAY="$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0"
-export DISPLAY
-
-(vcxsrv.exe :0 -silent-dup-error -ac -nowgl -dpms -terminate -lesspointer -multiwindow -clipboard &>/dev/null &)
-
-# Theme
-export GTK_THEME="windows-10-dark"
-export QT_STYLE_OVERRRIDE="windows-10-dark"
-# NOTE: Use lxappearance to set fonts/etc
 
 sudo prlimit -p "$$" --nofile=10000:10000
