ab740c031773b0e8b10701aa8bfcbb8be6115546
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2022-12-04 21:09:43 +0100

    Fix trailing indent for neovim

--- nix/home/editor.nix
+++ nix/home/editor.nix
@@ -160,7 +160,7 @@ in
               use_treesitter = true,
               use_treesitter_scope = true,
 
-              show_trailing_blankline_indent = true,
+              show_trailing_blankline_indent = false,
               show_first_indent_level = false,
 
               show_current_context = true,
