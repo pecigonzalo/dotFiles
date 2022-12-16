2fde0cbb6d9552d0d7b35929dbe4e9541c33bff5
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2022-12-16 12:22:59 -0300

    Send sshuttle back to stable old

--- nix/overlays/stable.nix
+++ nix/overlays/stable.nix
@@ -2,13 +2,13 @@ final: prev:
 {
   inherit (final.pkgs-21-05)
     dnsutils# https://github.com/NixOS/nixpkgs/issues/152056
+    sshuttle
     ;
   inherit (final.pkgs-21-11)
     pre-commit
     ;
   inherit (final.pkgs-stable)
     kcli
-    sshuttle
     ansible
     curlie
     starship
