2a5b955f958c27cc99fe4da406f49702ef039443
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2023-06-04 21:59:53 +0200

    Fix ignore .git

--- nix/home/neovim/explorer.lua
+++ nix/home/neovim/explorer.lua
@@ -1,7 +1,14 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 -- Explorer
 require("nvim-tree").setup({
   filters = {
-    custom = { ".git" },
+    custom = { "^\\.git$" },
   },
   hijack_cursor = true,
   renderer = {
