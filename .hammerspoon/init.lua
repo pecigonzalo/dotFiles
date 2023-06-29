4e71b833e2a4147c48b942577832f6f9c0ab53a7
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2023-06-29 15:05:13 +0200

    Draft work for hammerspoon based remap

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -6,7 +6,26 @@ hs.alert("Loading the Hammer")
 local hyper = require("hyper")
 local keymap = require("keymap")
 
+-- Hyper shortcuts
 for _, value in ipairs(keymap.hyper) do
   local mod, key, fn = table.unpack(value)
   hyper.bind(mod, key, fn)
 end
+
+-- Remaps
+-- for _, value in ipairs(keymap.remap) do
+--   local from, to, shouldEnable = table.unpack(value)
+--
+--   local fromMod, fromKey = table.unpack(from)
+--   local toMod, toKey = table.unpack(to)
+--
+--   local function wrapper()
+--     if shouldEnable and shouldEnable() then
+--       keymap.keyPress(toMod, toKey)
+--     else
+--       keymap.keyPress(fromMod, fromKey)
+--     end
+--   end
+--
+--   hs.hotkey.bind(fromMod, fromKey, nil, wrapper)
+--w end
--- .hammerspoon/keymap.lua
+++ .hammerspoon/keymap.lua
@@ -1,166 +1,227 @@
 local window = require("window")
 local chrome = require("chrome")
 
-local isInTerminal = function()
-  local app = hs.application.frontmostApplication():name()
-  return app == "iTerm2" or app == "Terminal" or app == "WezTerm"
+local M = {}
+
+function M.keyPress(mod, key)
+  print("Sending keystroke: " .. hs.inspect(mod) .. " " .. key)
+  hs.eventtap.keyStroke(mod, key, 1)
 end
 
-return {
-  hyper = {
-    {
-      {},
-      "R",
-      function()
-        hs.reload()
-      end,
-    },
-    {
-      {},
-      "Left",
-      function()
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
+local function Set(list)
+  local set = {}
+  for _, l in ipairs(list) do
+    set[l] = true
+  end
+  return set
+end
 
-        if f == window.up(win) then
-          win:setFrame(window.upLeft(win))
-          return
-        elseif f == window.upRight(win) then
-          win:setFrame(window.up(win))
-          return
-        elseif f == window.down(win) then
-          win:setFrame(window.downLeft(win))
-          return
-        elseif f == window.downRight(win) then
-          win:setFrame(window.down(win))
-          return
-        end
+local function isCurrentApp(apps)
+  local appSet = Set(apps)
+  local currentApp = hs.application.frontmostApplication():name()
 
-        win:setFrame(window.left(win))
-      end,
-    },
+  print("Current App: " .. currentApp)
 
-    {
-      {},
-      "Right",
-      function()
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
+  if appSet[currentApp] then
+    return true
+  end
+  return false
+end
 
-        if f == window.up(win) then
-          win:setFrame(window.upRight(win))
-          return
-        elseif f == window.upLeft(win) then
-          win:setFrame(window.up(win))
-          return
-        elseif f == window.down(win) then
-          win:setFrame(window.downRight(win))
-          return
-        elseif f == window.downLeft(win) then
-          win:setFrame(window.down(win))
-          return
-        end
+local function isRemote()
+  return isCurrentApp({ "Parsec" })
+end
 
-        win:setFrame(window.right(win))
-      end,
-    },
+local function isInTerminal()
+  return isCurrentApp({ "iTerm2", "Terminal", "WezTerm" })
+end
 
-    {
-      {},
-      "Up",
-      function()
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
+local function isEditor()
+  return isCurrentApp({ "Code" })
+end
 
-        if f == window.up(win) then
-          window.maximize(win)
-          return
-        elseif f == window.left(win) then
-          win:setFrame(window.upLeft(win))
-          return
-        elseif f == window.right(win) then
-          win:setFrame(window.upRight(win))
-          return
-        elseif f == window.downRight(win) then
-          win:setFrame(window.right(win))
-          return
-        elseif f == window.downLeft(win) then
-          win:setFrame(window.left(win))
-          return
-        end
+local general = {}
+for _, key in ipairs({ "w", "n", "t", "c", "x", "z", "v", "f", "a" }) do
+  table.insert(general, {
+    { { "ctrl" }, key },
+    { { "cmd" },  key },
+    function()
+      return not isCurrentApp({
+        "Code",
+        "WezTerm",
+        "Terminal",
+        "iTerm2",
+        "IntelliJ IDEA",
+        "Fleet",
+      })
+    end,
+  })
+end
 
-        win:setFrame(window.up(win))
-      end,
-    },
-
-    {
-      {},
-      "Down",
-      function()
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-
-        if f == window.left(win) then
-          win:setFrame(window.downLeft(win))
-          return
-        elseif f == window.right(win) then
-          win:setFrame(window.downRight(win))
-          return
-        elseif f == window.upRight(win) then
-          win:setFrame(window.right(win))
-          return
-        elseif f == window.upLeft(win) then
-          win:setFrame(window.left(win))
-          return
-        end
-
-        win:setFrame(window.down(win))
-      end,
-    },
-
-    -- Debug hotkey
-    {
-      {},
-      "`",
-      function()
-        local win = hs.window.focusedWindow()
-
-        print("x: " .. win:frame().x)
-        print("y: " .. win:frame().y)
-        print("w: " .. win:frame().w)
-        print("h: " .. win:frame().h)
-        -- print("left: x: " .. window.left(win).x .. " y: " .. window.left(win).y .. " w: " .. window.left(win).w .. "  h: " .. window.left(win).h)
-        -- print("right: x: " .. window.right(win).x .. " y: " .. window.right(win).y .. " w: " .. window.right(win).w ..  " h: " .. window.right(win).h)
-        -- print("up: x: " .. window.up(win).x .. " y: " .. window.up(win).y .. " w: " .. window.up(win).w .. "h:   " .. window.up(win).h)
-        -- print("down: x: " .. window.down(win).x .. " y: " .. window.down(win).y .. " w: " .. window.down(win).w .. "  h: " .. window.down(win).h)
-        -- print("upLeft: ")
-        -- print(window.upLeft(win))
-        -- print("upRight: ")
-        -- print(window.upRight(win))
-      end,
-    },
-
-    {
-      {},
-      "M",
-      function()
-        chrome.jump("meet.google.com")
-      end,
-    },
-
-    {
-      {},
-      "T",
-      function()
-        hs.application.open("WezTerm")
-      end,
-    },
-    {
-      {},
-      "S",
-      function()
-        hs.application.open("Slack")
-      end,
-    },
+M.remap = {
+  table.unpack(general),
+  {
+    { { "ctrl", "shift" }, "v" },
+    { { "cmd" },           "v" },
+    nil,
   },
 }
+
+M.hyper = {
+  {
+    {},
+    "R",
+    function()
+      hs.reload()
+    end,
+  },
+  {
+    {},
+    "Left",
+    function()
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+
+      if f == window.up(win) then
+        win:setFrame(window.upLeft(win))
+        return
+      elseif f == window.upRight(win) then
+        win:setFrame(window.up(win))
+        return
+      elseif f == window.down(win) then
+        win:setFrame(window.downLeft(win))
+        return
+      elseif f == window.downRight(win) then
+        win:setFrame(window.down(win))
+        return
+      end
+
+      win:setFrame(window.left(win))
+    end,
+  },
+
+  {
+    {},
+    "Right",
+    function()
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+
+      if f == window.up(win) then
+        win:setFrame(window.upRight(win))
+        return
+      elseif f == window.upLeft(win) then
+        win:setFrame(window.up(win))
+        return
+      elseif f == window.down(win) then
+        win:setFrame(window.downRight(win))
+        return
+      elseif f == window.downLeft(win) then
+        win:setFrame(window.down(win))
+        return
+      end
+
+      win:setFrame(window.right(win))
+    end,
+  },
+
+  {
+    {},
+    "Up",
+    function()
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+
+      if f == window.up(win) then
+        window.maximize(win)
+        return
+      elseif f == window.left(win) then
+        win:setFrame(window.upLeft(win))
+        return
+      elseif f == window.right(win) then
+        win:setFrame(window.upRight(win))
+        return
+      elseif f == window.downRight(win) then
+        win:setFrame(window.right(win))
+        return
+      elseif f == window.downLeft(win) then
+        win:setFrame(window.left(win))
+        return
+      end
+
+      win:setFrame(window.up(win))
+    end,
+  },
+
+  {
+    {},
+    "Down",
+    function()
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+
+      if f == window.left(win) then
+        win:setFrame(window.downLeft(win))
+        return
+      elseif f == window.right(win) then
+        win:setFrame(window.downRight(win))
+        return
+      elseif f == window.upRight(win) then
+        win:setFrame(window.right(win))
+        return
+      elseif f == window.upLeft(win) then
+        win:setFrame(window.left(win))
+        return
+      end
+
+      win:setFrame(window.down(win))
+    end,
+  },
+
+  -- Debug hotkey
+  {
+    {},
+    "`",
+    function()
+      local win = hs.window.focusedWindow()
+
+      print("x: " .. win:frame().x)
+      print("y: " .. win:frame().y)
+      print("w: " .. win:frame().w)
+      print("h: " .. win:frame().h)
+      -- print("left: x: " .. window.left(win).x .. " y: " .. window.left(win).y .. " w: " .. window.left(win).w .. "  h: " .. window.left(win).h)
+      -- print("right: x: " .. window.right(win).x .. " y: " .. window.right(win).y .. " w: " .. window.right(win).w ..  " h: " .. window.right(win).h)
+      -- print("up: x: " .. window.up(win).x .. " y: " .. window.up(win).y .. " w: " .. window.up(win).w .. "h:   " .. window.up(win).h)
+      -- print("down: x: " .. window.down(win).x .. " y: " .. window.down(win).y .. " w: " .. window.down(win).w .. "  h: " .. window.down(win).h)
+      -- print("upLeft: ")
+      -- print(window.upLeft(win))
+      -- print("upRight: ")
+      -- print(window.upRight(win))
+    end,
+  },
+
+  {
+    {},
+    "M",
+    function()
+      chrome.jump("meet.google.com")
+    end,
+  },
+
+  {
+    {},
+    "T",
+    function()
+      hs.application.open("WezTerm")
+    end,
+  },
+  {
+    {},
+    "S",
+    function()
+      hs.application.open("Slack")
+    end,
+  },
+}
+
+return M
